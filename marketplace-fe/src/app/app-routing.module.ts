1c12e643850184f02e4cd655f1e817c633dec919
David Del Sol <Dcastro@gap.com>
2022-03-29 11:15:33 -0600

    Fixed routes

--- marketplace-fe/src/app/app-routing.module.ts
+++ marketplace-fe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ShowProductsComponent } from './components/show-products/show-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ShowProductsComponent},
+  { path: 'create-product', component: CreateProductComponent },
+  { path: 'edit-product/:id', component: CreateProductComponent },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
